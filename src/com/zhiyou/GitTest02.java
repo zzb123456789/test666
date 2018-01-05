de378a9c4bd294b127b1aae2132905935e9fff9e
zzb <1270023916@qq.com>
2018-01-05 22:22:48 +0800

    update test

--- src/com/zhiyou/GitTest02.java
+++ src/com/zhiyou/GitTest02.java
@@ -2,6 +2,6 @@ package com.zhiyou;
 
 public class GitTest02 {
 	public static void main(String[] args) {
-		System.out.println("啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊");
+		System.out.println("噢噢噢噢哦哦哦哦哦哦");
 	}
 }
