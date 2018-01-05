a5dfe5ad4cafdfe6ba82754ae8f4f0e0bb0d0719
zzb123456789 <1270023916@qq.com>
2018-01-05 22:32:00 +0800

    update   000000000004

--- src/com/zhiyou/GitTest02.java
+++ src/com/zhiyou/GitTest02.java
@@ -2,6 +2,6 @@ package com.zhiyou;
 
 public class GitTest02 {
 	public static void main(String[] args) {
-		System.out.println("222222222222222");
+		System.out.println("8888888888888888888");
 	}
 }
